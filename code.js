cbd7ebc4f82d98f8ffab9c87bc9b9c25dd385a93
G-Morph <gk@morphelectronics.com>
2024-12-12 19:51:45 -0500

    add more functionality to the fibo stuff

--- code.js
+++ code.js
@@ -7,4 +7,13 @@ function ButtonStuff() {
     numberVar.innerHTML = nextStep;
     previousStep = nowStep;
     nowStep = nextStep;
+    if (nextStep % 2 == 0) {
+        document.getElementById("thangy").innerHTML = "THINGY!";
+    }
+    else if (nextStep % 3 == 0) {
+        document.getElementById("thangy").innerHTML = "THAAAAANGY!";
+    }
+    else {
+        document.getElementById("thangy").innerHTML = "thingy";
+    }
 }
